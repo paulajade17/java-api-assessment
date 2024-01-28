0f41f8a4e4c06cbee7a57506377940b21bf1e9f6
Paula Semakula <paulajade93@hotmail.co.uk>
2024-01-28 12:12:44 +0000

    create coffee repository interface

---
+++ src/main/java/com/cbfacademy/apiassessment/core/Repository.java
@@ -0,0 +1,41 @@
+package com.cbfacademy.apiassessment.core;
+
+import java.util.Optional;
+
+/**
+ * Simplified, partial implementation of the {@link org.springframework.data.repository.CrudRepository} interface
+ * @see <a href="https://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html#repositories.definition-tuning">Repository docs</a>
+ */
+public interface Repository<T, ID> extends org.springframework.data.repository.Repository<T, ID> {
+
+    /**
+     * Retrieves all entities from the repository.
+     *
+     * @return a list of all entities
+     */
+    Iterable<T> findAll();
+
+    /**
+     * Finds an entity by its unique identifier.
+     *
+     * @param id the identifier of the entity
+     * @return the found entity, or null if no such entity exists
+     */
+    Optional<T> findById(ID id);
+
+    /**
+     * Saves an entity (new or existing) in the repository.
+     *
+     * @param entity the {@code <T>} to save
+     * @return the saved entity
+     */
+    <S extends T> S save(S entity);
+
+    /**
+     * Deletes an entity from the repository based on its unique identifier.
+     *
+     * @param id the id of the entity to delete
+     */
+    void deleteById(ID id);
+
+}
