9c57234de73fc8ca8743b912e5f297070666ea2a
Paula Semakula <paulajade93@hotmail.co.uk>
2024-02-01 14:18:24 +0000

    update controller and repository

--- src/main/java/com/cbfacademy/apiassessment/core/Repository.java
+++ src/main/java/com/cbfacademy/apiassessment/core/Repository.java
@@ -6,10 +6,10 @@ public interface Repository<T, ID> extends org.springframework.data.repository.R
 
     /**
      * Retrieves all entities from the repository.
-     *
+     * Iterable represents a sequence of elements that can be looped through
      * @return a list of all entities
      */
-    Iterable<T> findAll();
+    Iterable<T> findAll() throws PersistenceException;
 
     /**
      * Finds an entity by its unique identifier.
@@ -17,7 +17,7 @@ public interface Repository<T, ID> extends org.springframework.data.repository.R
      * @param id the identifier of the entity
      * @return the found entity, or null if no such entity exists
      */
-    Optional<T> findById(ID id);
+    Optional<T> findById(ID id) throws IllegalArgumentException, PersistenceException;
 
     /**
      * Saves an entity (new or existing) in the repository.
@@ -25,13 +25,13 @@ public interface Repository<T, ID> extends org.springframework.data.repository.R
      * @param entity the {@code <T>} to save
      * @return the saved entity
      */
-    <S extends T> S save(S entity);
+    <S extends T> S save(S entity) throws IllegalArgumentException, PersistenceException;
 
     /**
      * Deletes an entity from the repository based on its unique identifier.
      *
      * @param id the id of the entity to delete
      */
-    void deleteById(ID id);
+    void deleteById(ID id) throws IllegalArgumentException, PersistenceException;
 
 }
