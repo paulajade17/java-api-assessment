7533083bede7da44af95d88395482597c4070bbd
Paula Semakula <paulajade93@hotmail.co.uk>
2024-02-09 00:30:43 +0000

    update comments on RunTimeException class

--- src/main/java/com/cbfacademy/apiassessment/coffee/CoffeeNotFoundException.java
+++ src/main/java/com/cbfacademy/apiassessment/coffee/CoffeeNotFoundException.java
@@ -1,5 +1,7 @@
 package com.cbfacademy.apiassessment.coffee;
 
+// Created a custom exception called CoffeeNotFoundException
+// This class has inhertited the RunTimeException methods and behaviour
 public class CoffeeNotFoundException extends RuntimeException{
     public CoffeeNotFoundException(String message) {
         super(message);
